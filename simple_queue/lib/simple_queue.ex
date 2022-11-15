68809dbf176619ad318475fba66047c593a35848
Charles Roth <charlieroth4@icloud.com>
2022-11-15 09:24:19 +0100

    add docs/comments for SimpleQueue worker module

--- simple_queue/lib/simple_queue.ex
+++ simple_queue/lib/simple_queue.ex
@@ -1,19 +1,61 @@
-# https://elixirschool.com/en/lessons/advanced/otp_supervisors
 defmodule SimpleQueue do
   use GenServer
 
-  def start_link(state \\ []) do
-    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  @doc """
+  After the supervisor has started it must know how to
+  start/stop/restart its children
+
+  Each child module should have a `child_spec/1` function
+  to define these behaviours
+  """
+  def child_spec(opts) do
+    %{
+      # Required. Used by the supervisor to identify the child specification
+      id: SimpleQueue,
+      # Required. The module/function/arguments to call when started by
+      # the supervisor
+      start: {__MODULE__, :start_link, [opts]},
+      # Optional. The child's behaviour during shutdown
+      shutdown: 5_000,
+      # Optional. Approach for handling child process crashes
+      restart: :permanent,
+      # Optional. Either `:worker` or `:supervisor`. Defaults to `:worker`
+      type: :worker
+    }
   end
 
   def init(state), do: {:ok, state}
 
+  # asynchronous functions
   def handle_cast({:enqueue, item}, state), do: {:noreply, [item | state]}
-  def handle_call(:queue, _from, state), do: {:reply, state, state}
-  def handle_call(:dequeue, _from, [item | state]), do: {:reply, item, state}
-  def handle_call(:dequeue, _from, []), do: {:reply, nil, []}
 
-  def enqueue(item), do: GenServer.cast(__MODULE__, {:enqueue, item})
-  def dequeue(), do: GenServer.call(__MODULE__, :dequeue)
-  def queue(), do: GenServer.call(__MODULE__, :queue)
+  # synchronous functions
+  def handle_call(:queue, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_call(:dequeue, _from, [item | state]) do
+   {:reply, item, state}
+  end
+
+  def handle_call(:dequeue, _from, []) do
+    {:reply, nil, []}
+  end
+
+  # Client API
+  def start_link(state \\ []) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  def dequeue() do
+    GenServer.call(__MODULE__, :dequeue)
+  end
+
+  def queue() do
+    GenServer.call(__MODULE__, :queue)
+  end
+
+  def enqueue(item) do
+    GenServer.cast(__MODULE__, {:enqueue, item})
+  end
 end
--- simple_queue/lib/simple_queue/application.ex
+++ simple_queue/lib/simple_queue/application.ex
@@ -10,7 +10,13 @@ defmodule SimpleQueue.Application do
       {SimpleQueue, [1, 2, 3]}
     ]
 
-    opts = [strategy: :one_for_one, name: SimpleQueue.Supervisor]
+    # https://elixirschool.com/en/lessons/advanced/otp_supervisors
+    opts = [
+      # strategy: :one_for_all, # restart all child processes in the event of a failure
+      # strategy: :rest_for_one, # restart the failed processes and any process started after it
+      strategy: :one_for_one, # only restart the failed child process
+      name: SimpleQueue.Supervisor
+    ]
     Supervisor.start_link(children, opts)
   end
 end
